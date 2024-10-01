e14bf8077664e8504726b9ff8bfa0a3c1c969372
Alex Basista <abasista@hashicorp.com>
2024-10-01 11:58:38 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ variable "pet_count" {
 variable "length" {
   type        = number
   description = "Length of random_pet."
-  default     = 20
+  default     = 100
 }
 
 resource "random_pet" "main" {
